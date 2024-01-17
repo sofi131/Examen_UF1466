3bd0230b7c4e1706781c729bc4ed42cc232cafa5
Sofía Sanmartín <91051075+sofi131@users.noreply.github.com>
2024-01-17 17:43:53 +0100

    Update Vehiculo.java

--- src/com/ceica/Modelos/Vehiculo.java
+++ src/com/ceica/Modelos/Vehiculo.java
@@ -1,2 +1,27 @@
-package com.ceica.Modelos;public class vehiculo {
+package com.ceica.Modelos;
+
+public class Vehiculo {
+    String marca;
+    String modelo;
+    int year_fabricacion;
+
+//constructor
+    public Vehiculo(String marca, String modelo, int year_fabricacion) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.year_fabricacion = year_fabricacion;
+    }
+//clase mostrar detalles
+    public void mostrarDetalles(){
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Año fabricación: " + year_fabricacion);
+
+    }
 }
+
+
+
+
+
+

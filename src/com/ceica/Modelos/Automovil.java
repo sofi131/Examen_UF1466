1ddb362fa3df1fc3664dbb565f497a5d34a2bae8
Sofía Sanmartín <91051075+sofi131@users.noreply.github.com>
2024-01-17 17:43:13 +0100

    Update Automovil.java

--- src/com/ceica/Modelos/Automovil.java
+++ src/com/ceica/Modelos/Automovil.java
@@ -1,2 +1,17 @@
-package com.ceica.Modelos;public class automovil {
+package com.ceica.Modelos;
+// 2ºpaso:hago los mismos pasos que vehículo motorizado y así con el resto
+public class Automovil extends VehiculoMotorizado{
+    int numPuertas;
+
+    public Automovil(String marca, String modelo, int year_fabricacion, String tipoCombustible, int numPuertas) {
+        super(marca, modelo, year_fabricacion, tipoCombustible);
+        this.numPuertas= numPuertas;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Número de puertas: " + numPuertas);
+    }
 }
+

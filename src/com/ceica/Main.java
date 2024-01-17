1b655455e4134d83448a6fc08341dbfe94c3c4e0
Sofía Sanmartín <91051075+sofi131@users.noreply.github.com>
2024-01-17 17:42:45 +0100

    Update Main.java

--- src/com/ceica/Main.java
+++ src/com/ceica/Main.java
@@ -1,5 +1,24 @@
+package com.ceica;
+
+import com.ceica.Modelos.Automovil;
+import com.ceica.Modelos.Motocicleta;
+import com.ceica.Modelos.Vehiculo;
+import com.ceica.Modelos.VehiculoMotorizado;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vehiculo vehiculo = new Vehiculo("Toyota","Azul",2024);
+        //la clase para mostrar
+        vehiculo.mostrarDetalles();
+        //motorizado
+        VehiculoMotorizado VehiculoMotorizado= new VehiculoMotorizado("Citroen", "Rojo", 2024,"gasolina");
+        VehiculoMotorizado.mostrarDetalles();
+        //automovil
+        Automovil automovil = new Automovil("Ford", "Fiesta", 2024,"gasolina", 2);
+        automovil.mostrarDetalles();
+        //moto
+        Motocicleta motocicleta = new Motocicleta("Seat", "Ibiza", 2024,"gasolina","rosa");
+        motocicleta.mostrarDetalles();
+
     }
 }

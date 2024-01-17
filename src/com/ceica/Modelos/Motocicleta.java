7b17fb173ea8474aab1a9dc90cbcf2030d1c7daa
Sofía Sanmartín <91051075+sofi131@users.noreply.github.com>
2024-01-17 17:43:34 +0100

    Update Motocicleta.java

--- src/com/ceica/Modelos/Motocicleta.java
+++ src/com/ceica/Modelos/Motocicleta.java
@@ -1,2 +1,17 @@
-package com.ceica.Modelos;public class motocicleta {
+package com.ceica.Modelos;
+
+public class Motocicleta extends VehiculoMotorizado{
+    String tipoMotocicleta;
+
+    public Motocicleta(String marca, String modelo, int year_fabricacion, String tipoCombustible, String tipoMotocicleta) {
+        super(marca, modelo, year_fabricacion, tipoCombustible);
+        this.tipoMotocicleta=tipoMotocicleta;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Tipo de motocicleta: " + tipoMotocicleta);
+    }
+
 }
